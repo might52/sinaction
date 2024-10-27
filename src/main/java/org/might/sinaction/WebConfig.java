cb4ac46e43d615ec457bd552e1b572fac8c06969
Andrei F._ <answers_com@hotmail.com>
2024-10-27 19:36:18 +0300

    added handling forms

---
+++ src/main/java/org/might/sinaction/WebConfig.java
@@ -0,0 +1,13 @@
+package org.might.sinaction;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
