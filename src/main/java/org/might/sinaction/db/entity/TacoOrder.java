d323c267cc6cae320f450f12f80967a0a401a35f
Andrei F._ <answers_com@hotmail.com>
2024-10-27 23:32:56 +0300

    removed jdbc

--- src/main/java/org/might/sinaction/db/entity/TacoOrder.java
+++ src/main/java/org/might/sinaction/db/entity/TacoOrder.java
@@ -5,18 +5,24 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Table
 public class TacoOrder implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
     private Long id;
-    private Date placedAt;
+    private Date placedAt = new Date();
 
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
--- src/main/java/org/might/sinaction/db/repositories/OrderRepository.java
+++ src/main/java/org/might/sinaction/db/repositories/OrderRepository.java
@@ -1,7 +1,7 @@
 package org.might.sinaction.db.repositories;
 
 import org.might.sinaction.db.entity.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 }
