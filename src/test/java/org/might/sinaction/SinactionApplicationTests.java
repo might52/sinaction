e1e801b969ded378c3b5f0289df11bb29d02ae57
Andrei F._ <answers_com@hotmail.com>
2024-10-27 14:28:01 +0300

    init commit

---
+++ src/test/java/org/might/sinaction/SinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.might.sinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
