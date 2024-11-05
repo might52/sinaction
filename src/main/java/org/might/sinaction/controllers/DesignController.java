4be64aa1568fff74a6f6d2aaf46c2d614a04f9e1
Andrei F._ <answers_com@hotmail.com>
2024-11-06 00:52:55 +0300

    added login and corrected the logic for csrf

--- src/main/java/org/might/sinaction/controllers/DesignController.java
+++ src/main/java/org/might/sinaction/controllers/DesignController.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.might.sinaction.db.entity.Ingredient;
 import org.might.sinaction.db.entity.Taco;
 import org.might.sinaction.db.entity.TacoOrder;
+import org.might.sinaction.db.entity.User;
 import org.might.sinaction.db.repositories.IngredientRepository;
+import org.might.sinaction.db.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 @Slf4j
@@ -25,19 +30,28 @@ import java.util.stream.StreamSupport;
 public class DesignController {
 
     private final IngredientRepository ingredientRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public DesignController(IngredientRepository ingredientRepository) {
+    public DesignController(IngredientRepository ingredientRepository, UserRepository userRepository) {
         this.ingredientRepository = ingredientRepository;
+        this.userRepository = userRepository;
     }
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        Iterable<Ingredient> ingredients = ingredientRepository.findAll();
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredients::add);
         Ingredient.Type[] types = Ingredient.Type.values();
         for (Ingredient.Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
+    }
+
+    @ModelAttribute(name = "user")
+    public User user(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findByUsername(username);
     }
 
     @ModelAttribute(name = "tacoOrder")
